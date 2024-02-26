905efc910b8b0aa4472385cb18a1c1730164ab92
Piyush Kumar <mr.piyush.cv@gmail.com>
2024-02-26 19:32:55 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors :
+      {
+        "acc-org" : "#ff971e"
+      }
+    },
+  },
+  plugins: [],
+}
